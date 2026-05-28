baff9b8a853dde61818b37be89a99b6d16ba3924
Riju Vijayan <rijuvijayan@gmail.com>
2026-05-28 11:50:33 +0530

    feat: add e2e flow of prompt and llm

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,10 +1,17 @@
+import os
+from encodings import search_function
 from importlib import metadata
 
 from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from openai import max_retries
 
 load_dotenv()
 
@@ -39,6 +46,10 @@ docs = [
         "question": "How do I reset my online banking password?",
         "answer": "Click the 'Forgot Password' link on the login page, enter your registered email address, and follow the instructions sent to your inbox. For security reasons, the reset link expires in 15 minutes.",
     },
+    {
+        "question": "what is your Leadership team",
+        "answer": "Fincorp is headed by Sundar Pichai, with Raghuram rajan as the CFO",
+    },
 ]
 
 
@@ -56,16 +67,58 @@ def chroma_db():
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
     doc_chunks = text_splitter.split_documents(documents)
 
+    CHROMA_PATH = "./chroma_db"
+
     # store it in the vector db (e.g. chromadb)
-    vector_db = Chroma.from_documents(documents=doc_chunks, embedding=model)
-    print(
-        f"documents created in vector with {vector_db._collection.count()} documents."
-    )
+    if not os.path.exists(CHROMA_PATH):
+        print("chromadb not found. creating...")
+        vector_db = Chroma.from_documents(
+            documents=doc_chunks, embedding=model, persist_directory=CHROMA_PATH
+        )
+        print(
+            f"chromadb created in {CHROMA_PATH} with {vector_db._collection.count()} documents."
+        )
+    else:
+        vector_db = Chroma(embedding_function=model, persist_directory=CHROMA_PATH)
+        print(
+            f"documents are read from {CHROMA_PATH} with {vector_db._collection.count()} documents."
+        )
 
     # print answers
     results = vector_db.similarity_search_with_score("how secure is your bank?", k=1)
     for doc, score in results:
         print(f"Score: {score:.4f}\n{doc.page_content}\n")
+
+    print("=== now as retriever ===")
+    results_as_retriever = vector_db.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    )
+
+    # retriever_result = results_as_retriever.invoke("how secure is your bank?")
+    # for doc in retriever_result:
+    #     print(doc.page_content)
+
+    prompt = ChatPromptTemplate.from_template("""
+        You are an useful assistant. Answer using the provided context. if you do not know the answer
+        just say so. 
+
+        context: {context}
+        question: {question}
+    """)
+
+    llm = init_chat_model(model="gpt-4o-mini", max_retries=2)
+
+    chain = (
+        {"context": results_as_retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    question = input("ask your question: ")
+    result = chain.invoke(question)
+
+    print(result)
 
 
 if __name__ == "__main__":
